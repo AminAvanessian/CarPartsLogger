316962f244d855c0b50342aaeef84632a8d51d24
Amin-Avanessian <amin.avanessianzadeh@uoit.net>
2019-04-19 20:33:18 -0400

    added parts class

---
+++ CarPart.java
@@ -0,0 +1,19 @@
+class CarPart {
+    // The fields include Part ID, Make, Model, and Year.
+    Int partID;
+    CarMake make;
+    String model;
+    Int year;
+}
+
+enum CarMake {
+    CHEVROLET,
+    FORD,
+    HONDA,
+    HYUNDAI,
+    KIA,
+    MAZDA,
+    MITSUBISHI,
+    TOYOTA,
+    VOLKSWAGEN,
+}
